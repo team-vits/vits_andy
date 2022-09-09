643dc9bc79ae4a20769317386960e063067dc7ea
“DiegoCol93” <dfelo93@gmail.com>
2022-09-09 14:32:42 -0500

    FIXED flake8 linting errors.

--- app/home_page/views.py
+++ app/home_page/views.py
@@ -6,4 +6,4 @@ import random
 def home_view(request):
     """ Home page view method definition """
     data = {'test': random.randrange(1, 100)}
-    return(render(request, "home.html", data))
+    return (render(request, "home.html", data))
